e32a3de7013d26bb4c8689a340ea154186508a64
Camryn Douglass <camryndouglass@MacBook-Air-6.lan>
2023-07-27 18:20:23 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const array = [2,4,6,8,11,20,15,22]
+const odd = [];
+
+for (let i=0; i < array.length; i++) {
+    const num = array[i];
+
+    if (num % 2 === 1) {
+        odd.push(num);
+    }
+}
+
+console.log(odd);
+
+const array = [2,4,6,8,11,20,15,22]
+const odd = [];
+
+for (let i=0; i < array.length; i++) {
+    const num = array[i];
+
+    if (num % 2 === 1) {
+        odd.push(num);
+    }
+}
+
+console.log(odd);
